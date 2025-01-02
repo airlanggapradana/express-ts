84969af875ba81b8d7559bf87d6e03910c8a56e1
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-01-02 16:18:22 +0700

    feat: add JWT authentication to login functionality and enhance request handling

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import prisma from "../../prisma/prisma";
 import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const login = async (req: Request, res: Response) => {
   try {
@@ -18,9 +19,13 @@ export const login = async (req: Request, res: Response) => {
 
       if (isPasswordMatch) {
         const { password, ...rest } = existingUser;
+        const accessToken = jwt.sign(rest, process.env.JWT_SECRET_KEY!, {
+          algorithm: "HS256",
+          expiresIn: "1h",
+        });
         res.status(200).send({
           message: "User logged in successfully",
-          result: rest,
+          result: { rest, accessToken },
         });
       } else {
         res.status(401).send({
--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/products", products);
 app.use("/users", users);
